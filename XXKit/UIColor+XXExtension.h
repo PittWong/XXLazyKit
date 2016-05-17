da30ed326a80717c0ba4ca5c1d5a5f90b5e156a6
PittWong <1030046166@qq.com>
2016-05-17 17:17:57 +0800

    color应该是与yykit冲突

--- XXKit/UIColor+XXExtension.h
+++ XXKit/UIColor+XXExtension.h
@@ -19,16 +19,16 @@
 
 
 @interface UIColor (XXExtension)
-
-+ (UIColor *) colorWithHexString: (NSString *)color;
-
-+ (UIColor *) colorWithHexString: (NSString *)color Alpha:(CGFloat)alpha;
+//
+//+ (UIColor *) colorWithHexString: (NSString *)color;
+//
+//+ (UIColor *) colorWithHexString: (NSString *)color Alpha:(CGFloat)alpha;
 
 + (UIColor *) randomColor;
 
-+ (UIColor *) textColor333333;          //333333
-+ (UIColor *) textColor666666;          //666666
-+ (UIColor *) textColor7c7c7c;          //7c7c7c
-+ (UIColor *) textColor999999;          //999999
-+ (UIColor *) textColoraaaaaa;          //aaaaaa
+//+ (UIColor *) textColor333333;          //333333
+//+ (UIColor *) textColor666666;          //666666
+//+ (UIColor *) textColor7c7c7c;          //7c7c7c
+//+ (UIColor *) textColor999999;          //999999
+//+ (UIColor *) textColoraaaaaa;          //aaaaaa
 @end
