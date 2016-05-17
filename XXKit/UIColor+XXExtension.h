bed3a88471b3c4f71dbd1b8a65086763e205cecd
PittWong <1030046166@qq.com>
2016-05-17 17:28:21 +0800

    解决color冲突

--- XXKit/UIColor+XXExtension.h
+++ XXKit/UIColor+XXExtension.h
@@ -8,27 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-//颜色模型
-@interface ColorModel : NSObject
-@property (nonatomic,assign) int R;
-@property (nonatomic,assign) int G;
-@property (nonatomic,assign) int B;
-@property (nonatomic,assign) CGFloat alpha;
-@end
-
-
-
 @interface UIColor (XXExtension)
-//
-//+ (UIColor *) colorWithHexString: (NSString *)color;
-//
-//+ (UIColor *) colorWithHexString: (NSString *)color Alpha:(CGFloat)alpha;
+
++ (UIColor *) colorWithHexString: (NSString *)color;
+
++ (UIColor *) colorWithHexString: (NSString *)color Alpha:(CGFloat)alpha;
 
 + (UIColor *) randomColor;
 
-//+ (UIColor *) textColor333333;          //333333
-//+ (UIColor *) textColor666666;          //666666
-//+ (UIColor *) textColor7c7c7c;          //7c7c7c
-//+ (UIColor *) textColor999999;          //999999
-//+ (UIColor *) textColoraaaaaa;          //aaaaaa
++ (UIColor *) textColor333333;          //333333
++ (UIColor *) textColor666666;          //666666
++ (UIColor *) textColor7c7c7c;          //7c7c7c
++ (UIColor *) textColor999999;          //999999
++ (UIColor *) textColoraaaaaa;          //aaaaaa
 @end
