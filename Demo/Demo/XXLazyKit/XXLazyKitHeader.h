15e1a4c32e21d8f6ff0f66bea7bb68c681f9597a
PittWong <1030046166@qq.com>
2016-05-18 10:00:48 +0800

    刷新头文件

---
+++ Demo/Demo/XXLazyKit/XXLazyKitHeader.h
@@ -0,0 +1,40 @@
+
+
+
+
+
+#if __has_include(<XXLazyKit/XXLazyKitHeader.h>)
+
+FOUNDATION_EXPORT double XXKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char XXKitVersionString[];
+
+#import <XXLazyKit/NSDate+XXExtension.h>
+#import <XXLazyKit/NSString+XXExtension.h>
+#import <XXLazyKit/UIBarButtonItem+XXExtension.h>
+#import <XXLazyKit/UIButton+XXExtension.h>
+#import <XXLazyKit/UIColor+XXExtension.h>
+#import <XXLazyKit/UIFont+XXExtension.h>
+#import <XXLazyKit/UILabel+XXExtension.h>
+#import <XXLazyKit/UIView+XXExtension.h>
+
+#import <XXLazyKit/XXLazySetView.h>
+#import <XXLazyKit/XXLazyCreateView.h>
+
+
+#else
+
+#import "NSDate+XXExtension.h"
+#import "NSString+XXExtension.h"
+#import "UIBarButtonItem+XXExtension.h"
+#import "UIButton+XXExtension.h"
+#import "UIColor+XXExtension.h"
+#import "UIFont+XXExtension.h"
+#import "UILabel+XXExtension.h"
+#import "UIView+XXExtension.h"
+
+#import "XXLazySetView.h"
+#import "XXLazyCreateView.h"
+
+
+
+#endif
